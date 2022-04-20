b0fb6cc41c62a4a9ca3af69772141c4ba78cc938
Kevin Peset <kevin.peset@gmail.com>
2022-04-20 14:22:13 +0200

    US_01 Clickable bouton + US_21 Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,33 @@
 import Home from "@pages/Home";
+import GetMovie from "@components/GetMovie"
+import axios from "axios";
 
 import "./App.css";
 
 function App() {
+  const apiKey = "8b3e8af5c0e9e0a359483a16acf719e2"
+  const idFilm = "550"
+  const UrlApi = `https://api.themoviedb.org/3/movie/${idFilm}?api_key=${apiKey}`
+
+  let ficheFilm = {};
+  axios
+    .get(UrlApi)
+    .then((response) => response.data)
+    .then((data) => {
+      ficheFilm = { "titre": data.title, "affiche": `https://image.tmdb.org/t/p/w500/${data.poster_path}` };
+      console.log(ficheFilm)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  console.log(`test ${ficheFilm}`)
+
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      Coucou
+      {ficheFilm.titre}
+      Test
     </div>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,21 @@
-import Counter from "@components/Counter";
-import logo from "@assets/logo.svg";
+import React from "react";
+import "../App.css";
+import Carousel from "../components/Carousel";
 
-export default function Home() {
+function Home() {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello Vite + React !</p>
-
-      <Counter />
-
-      <p>
-        Edit <code>App.jsx</code> and save to test HMR updates.
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-      </p>
-    </header>
+    <div className="screen">
+      <div className="title_bar">
+        <h1>MOVIZZ</h1>
+        <Carousel />
+        <div className="container_button">
+          <button type="button" className="play_button">
+            JOUER
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
+
+export default Home;
