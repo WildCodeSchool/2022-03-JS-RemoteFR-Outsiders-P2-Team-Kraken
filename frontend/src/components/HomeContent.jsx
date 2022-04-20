95692e50a1f182cc82881153f42aefddb79c4ae6
Kevin Peset <kevin.peset@gmail.com>
2022-04-20 17:10:48 +0200

    Ajout du composant HomeContent

--- frontend/src/components/HomeContent.jsx
+++ frontend/src/components/HomeContent.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import {
+  faQuestionCircle,
+  faVolumeMute,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Carousel from "./Carousel";
+
+import "../App.css";
 
 function HomeContent() {
   return (
     <div>
       <Carousel />
       <div className="container_button">
-        <button type="button" className="play_button">
-          JOUER
-        </button>
+        <div className="try">
+          <FontAwesomeIcon className="sound_icon" icon={faVolumeMute} />
+          <button type="button" className="play_button">
+            JOUER
+          </button>
+          <FontAwesomeIcon className="help_icon" icon={faQuestionCircle} />
+        </div>
       </div>
     </div>
   );
