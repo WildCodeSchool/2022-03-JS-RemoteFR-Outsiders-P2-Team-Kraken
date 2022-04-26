49cc21b5dfadbeec79ddc6982e8f82b8dea4d1f9
AUDREN FLOCH <a.floch@outlook.fr>
2022-04-26 11:09:44 +0200

    DND ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,10 @@
 import React from "react";
-import genFilm from "./utils/fonctions";
+import Dnd from "./pages/5-films";
 
 import "./App.css";
 
 function App() {
-  // On génère un film aléatoire grâce à genFilm
-  const ficheFilm = genFilm(14);
-  // On génère un second film aléatoire grâce à genFilm
-  const ficheFilm2 = genFilm(14);
-  // On affiche ce qu'on veut
-  return (
-    <div className="App">
-      film 1 : {ficheFilm.titre} - film 2 : {ficheFilm2.titre}
-    </div>
-  );
+  return <Dnd />;
 }
 
 export default App;
