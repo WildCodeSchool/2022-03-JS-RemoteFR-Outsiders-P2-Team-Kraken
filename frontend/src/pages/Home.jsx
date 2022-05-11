179ecc50742134469efa600a3bde1047d52aa984
Sebastien Puigrenier <sebastien.puigrenier@gmail.com>
2022-05-11 09:30:25 +0200

    DEBUG space and tab in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -26,64 +26,64 @@ function Home() {
   };
 
   return (
-   <QuestionContextProvider>
-     <SoundContextProvider>
-       <ScoreContextProvider>
-        <Timer5ContextProvider>
-          <TimerContextProvider>
-            <div className="screen">
-              <TitleBar title={titleMain} />
-              <div className="game_content" style={{ width: "100%" }}>
-                <Routes>
-                  <Route path="/" element={<HomeContent />} />
-                  <Route
-                    path="/ScoreScreen"
-                    element={
-                      <ScoreScreen
-                        updateTitleMain={updateTitleMain}
-                        pseudo={pseudo}
-                      />
-                    }
-                  />
-                  <Route
-                    path="/configuration"
-                    element={
-                      <Configuration
-                        pseudo={pseudo}
-                        handlePseudoChange={handlePseudoChange}
-                      />
-                    }
-                  />
-                  <Route
-                    path="/game/two-movies"
-                    element={
-                      <TwoMovies
-                        updateTitleMain={updateTitleMain}
-                        titleMain={titleMain}
-                      />
-                    }
-                  />
-                  <Route
-                    path="/game/five-movies"
-                    element={
-                      <Dnd
-                        updateTitleMain={updateTitleMain}
-                        titleMain={titleMain}
-                      />
-                    }
-                  />
-                  <Route
-                    path="/*"
-                    element={
-                      <Page404
-                        updateTitleMain={updateTitleMain}
-                        titleMain={titleMain}
-                      />
-                    }
-                  />
-                </Routes>
+    <QuestionContextProvider>
+      <SoundContextProvider>
+        <ScoreContextProvider>
+          <Timer5ContextProvider>
+            <TimerContextProvider>
+              <div className="screen">
+                <TitleBar title={titleMain} />
+                <div className="game_content" style={{ width: "100%" }}>
+                  <Routes>
+                    <Route path="/" element={<HomeContent />} />
+                    <Route
+                      path="/ScoreScreen"
+                      element={
+                        <ScoreScreen
+                          updateTitleMain={updateTitleMain}
+                          pseudo={pseudo}
+                        />
+                      }
+                    />
+                    <Route
+                      path="/configuration"
+                      element={
+                        <Configuration
+                          pseudo={pseudo}
+                          handlePseudoChange={handlePseudoChange}
+                        />
+                      }
+                    />
+                    <Route
+                      path="/game/two-movies"
+                      element={
+                        <TwoMovies
+                          updateTitleMain={updateTitleMain}
+                          titleMain={titleMain}
+                        />
+                      }
+                    />
+                    <Route
+                      path="/game/five-movies"
+                      element={
+                        <Dnd
+                          updateTitleMain={updateTitleMain}
+                          titleMain={titleMain}
+                        />
+                      }
+                    />
+                    <Route
+                      path="/*"
+                      element={
+                        <Page404
+                          updateTitleMain={updateTitleMain}
+                          titleMain={titleMain}
+                        />
+                      }
+                    />
+                  </Routes>
+                </div>
               </div>
-            </div>
             </TimerContextProvider>
           </Timer5ContextProvider>
         </ScoreContextProvider>
